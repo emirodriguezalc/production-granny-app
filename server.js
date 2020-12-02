19b87b3de32fc0798a85ed5cf82c65b531a63d21
carlos-ch <carlos.tschavez@gmail.com>
2020-12-02 18:12:32 +0100

    deployment config

--- server.js
+++ server.js
@@ -1,25 +1,26 @@
-require("dotenv").config();
-const express = require("express");
+require('dotenv').config();
+const express = require('express');
 const app = express();
-const cors = require("cors");
-const mongoose = require("mongoose");
-const authRoute = require("./routes/auth");
-const postRoute = require("./routes/posts");
+const cors = require('cors');
+const mongoose = require('mongoose');
+const authRoute = require('./routes/auth');
+const postRoute = require('./routes/posts');
 mongoose.connect(
   process.env.DB_CONNECT,
   { useUnifiedTopology: true, useNewUrlParser: true },
-  () => console.log("connected to db")
+  () => console.log('connected to db')
 );
 
 app.use(express.json());
 app.use(
   cors({
-    origin: "http://localhost:3000", // <-- location of the react app were connecting to
+    origin: 'http://localhost:3000', // <-- location of the react app were connecting to
     credentials: true,
   })
 );
 
-app.use("/api/posts", postRoute);
+app.use('/api/posts', postRoute);
 
-app.use("/api/user", authRoute);
-app.listen(3002, () => console.log("Server Up"));
+app.use('/api/user', authRoute);
+const port = process.env.PORT || 3002;
+app.listen(port, () => console.log('Server Up'));
